9c02369b521664b9fea96352367550133e6ef4ae
zongsheng <xuzongsheng@foxmail.com>
2016-01-19 11:13:05 +0800

    add code automatic memory

--- static/js/index.js
+++ static/js/index.js
@@ -26,9 +26,10 @@ var codeHelper = {
             },2500);
             return false;
         };
-        $.ajax({
-            url:"run.php?token=59f9e9cf651d84d8c4065e7dc12a1fa9",
+        var ajaxRunObj = $.ajax({
+            url:"ajaxRun.php?token=6e12c60a96ad6517471294007f663ec4",
             type:"post",
+            timeout:1000,
             cache:false,
             data:{code:code},
             success:function(data){
@@ -45,8 +46,12 @@ var codeHelper = {
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 var status = XMLHttpRequest.status;
+                console.log(status);
                 if (status == 404) {
-                    msgTip('请配置run.php','error');
+                    msgTip('请配置ajaxRun.php','error');
+                } else if (textStatus=='timeout'){
+                    ajaxRunObj.abort();
+                    msgTip('运行超时，请检查！','error');
                 } else {
                     msgTip('运行失败，请检查！','error');
                 }
@@ -129,6 +134,76 @@ function msgTip(content,type) {
         $('#msgTip').fadeOut();
     },2500);
 }
+
+codeHelper.localData = {
+    userData: null,
+    name: location.hostname,
+    isLocalStorage: typeof localStorage == 'undefined'?false:true,
+    init: function(){
+        if (!this.userData) {
+            try {
+                this.userData = document.createElement('INPUT');
+                this.userData.type = "hidden";
+                this.userData.style.display = "none";
+                this.userData.addBehavior ("#default#userData");
+                document.body.appendChild(this.userData);
+                var expires = new Date();
+                expires.setDate(expires.getDate()+365);
+                this.userData.expires = expires.toUTCString();
+            } catch(e) {
+                return false;
+            }
+        }
+        return true;
+    },
+    set: function(key, value) {
+        if(this.isLocalStorage){
+            localStorage.setItem(key, value);
+        }
+        else if(this.init()){
+            this.userData.load(this.name);
+            this.userData.setAttribute(key, value);
+            this.userData.save(this.name);
+        }
+    },
+    get: function(key) {
+        if(this.isLocalStorage){
+            return localStorage.getItem(key);
+        }
+        else if(this.init()){
+            this.userData.load(this.name);
+            return this.userData.getAttribute(key)
+        }
+        return null;
+    },
+    remove: function(key) {
+        if(this.isLocalStorage){
+            localStorage.removeItem(key);
+        }
+        else if(this.init()){
+            this.userData.load(this.name);
+            this.userData.removeAttribute(key);
+            this.userData.save(this.name);
+        }
+    }
+};
+
+//定时将代码保存到本地空间里面
+setInterval(function(){
+    var code = editor.getValue();
+    if (code) {
+        codeHelper.localData.set('code',code);
+    }
+},2000);
+
+//初始化本地保存的代码
+(function() {
+    var code = codeHelper.localData.get('code');
+    if (code) {
+        codeHelper.insertContent(code, 1, 0);
+    }
+}());
+
 
 //北京时间
 (function startTime() {
